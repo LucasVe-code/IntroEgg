686d49fc7117c6daa2a564923795ea3de31cb52c
Lucas <lucasve96@gmail.com>
2023-06-01 16:12:41 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert(){
+    alert('Esta es una alerta JS')
+}
+
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click", function() {
+document.querySelector(".nav-links").
+classList.toggle("nav-links-responsive")})
